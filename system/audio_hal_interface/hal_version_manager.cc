f9de685ffab6ea2ee0ed9cb9c6f6f8d96d2eba9c
Peter Cai <peter@typeblog.net>
2022-08-24 15:45:18 -0400

    audio_hal_interface: Optionally use sysbta HAL
    Required to support sysbta, our system-side bt audio implementation.
    
    Change-Id: I59973e6ec84c5923be8a7c67b36b2e237f000860

--- system/audio_hal_interface/hal_version_manager.cc
+++ system/audio_hal_interface/hal_version_manager.cc
@@ -26,6 +26,7 @@
 #include "aidl/audio_aidl_interfaces.h"
 #include "include/check.h"
 #include "os/log.h"
+#include "osi/include/properties.h"
 
 namespace bluetooth {
 namespace audio {
@@ -35,6 +36,12 @@ using ::aidl::android::hardware::bluetooth::audio::
 
 static const std::string kDefaultAudioProviderFactoryInterface =
     std::string() + IBluetoothAudioProviderFactory::descriptor + "/default";
+static const std::string kSystemAudioProviderFactoryInterface =
+    std::string() + IBluetoothAudioProviderFactory::descriptor + "/sysbta";
+static inline const std::string& audioProviderFactoryInterface() {
+  return osi_property_get_bool("persist.bluetooth.system_audio_hal.enabled", false)
+    ? kSystemAudioProviderFactoryInterface : kDefaultAudioProviderFactoryInterface;
+}
 
 std::string toString(BluetoothAudioHalTransport transport) {
   switch (transport) {
@@ -76,7 +83,7 @@ BluetoothAudioHalVersion GetAidlInterfaceVersion() {
     int version = 0;
     auto provider_factory = IBluetoothAudioProviderFactory::fromBinder(
         ::ndk::SpAIBinder(AServiceManager_waitForService(
-            kDefaultAudioProviderFactoryInterface.c_str())));
+            audioProviderFactoryInterface().c_str())));
 
     if (provider_factory == nullptr) {
       LOG_ERROR(
@@ -146,7 +153,7 @@ HalVersionManager::GetProvidersFactory_2_0() {
 HalVersionManager::HalVersionManager() {
   hal_transport_ = BluetoothAudioHalTransport::UNKNOWN;
   if (AServiceManager_checkService(
-          kDefaultAudioProviderFactoryInterface.c_str()) != nullptr) {
+          audioProviderFactoryInterface().c_str()) != nullptr) {
     hal_version_ = GetAidlInterfaceVersion();
     hal_transport_ = BluetoothAudioHalTransport::AIDL;
     return;
