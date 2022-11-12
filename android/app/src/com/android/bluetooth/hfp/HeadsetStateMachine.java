e717a2ffebbc44056cb3cff2a3331e043cb3472b
Andreas Schneider <asn@cryptomilk.org>
2022-11-12 00:35:46 +0000

    On Samsung devices, we need to tell Audio HAL if we're running narrow band or wide band
    Ported to Android 13.
    
    Change-Id: I7802b7a29c017a2cd7018e82772183df1dfa0b89

--- android/app/src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ android/app/src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -147,6 +147,7 @@ public class HeadsetStateMachine extends StateMachine {
     // Audio Parameters
     private boolean mHasNrecEnabled = false;
     private boolean mHasWbsEnabled = false;
+    private int mReportScoSampleRate = 0;
     // AT Phone book keeps a group of states used by AT+CPBR commands
     @VisibleForTesting
     final AtPhonebook mPhonebook;
@@ -228,6 +229,7 @@ public class HeadsetStateMachine extends StateMachine {
         }
         mHasWbsEnabled = false;
         mHasNrecEnabled = false;
+        mReportScoSampleRate = 0;
     }
 
     public void dump(StringBuilder sb) {
@@ -457,6 +459,7 @@ public class HeadsetStateMachine extends StateMachine {
             mNeedDialingOutReply = false;
             mHasWbsEnabled = false;
             mHasNrecEnabled = false;
+            mReportScoSampleRate = 0;
             broadcastStateTransitions();
             // Remove the state machine for unbonded devices
             if (mPrevState != null
@@ -1542,6 +1545,9 @@ public class HeadsetStateMachine extends StateMachine {
                 + " hasNrecEnabled=" + mHasNrecEnabled
                 + " hasWbsEnabled=" + mHasWbsEnabled);
         am.setBluetoothHeadsetProperties(getCurrentDeviceName(), mHasNrecEnabled, mHasWbsEnabled);
+        if (mReportScoSampleRate > 0) {
+            am.setParameters("g_sco_samplerate=" + mReportScoSampleRate);
+        }
     }
 
     @VisibleForTesting
@@ -1685,10 +1691,12 @@ public class HeadsetStateMachine extends StateMachine {
         switch (wbsConfig) {
             case HeadsetHalConstants.BTHF_WBS_YES:
                 mHasWbsEnabled = true;
+                mReportScoSampleRate = 16000;
                 break;
             case HeadsetHalConstants.BTHF_WBS_NO:
             case HeadsetHalConstants.BTHF_WBS_NONE:
                 mHasWbsEnabled = false;
+                mReportScoSampleRate = 8000;
                 break;
             default:
                 Log.e(TAG, "processWBSEvent: unknown wbsConfig " + wbsConfig);
